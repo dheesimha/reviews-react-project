cc4639a05fb514f6497f4e270ee06d0513339455
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-07-04 10:46:31 +0530

    Add next and previous review buttons

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,58 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  console.log(people);
+  const { id, name, image, job, text } = people[index];
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    } else {
+      return number;
+    }
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft onClick={prevPerson} />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight onClick={nextPerson} />
+        </button>
+      </div>
+      <button className="random-btn">Surprise me</button>
+    </article>
+  );
 };
 
 export default Review;
